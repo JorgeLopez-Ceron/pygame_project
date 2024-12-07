efdb247ec27076fe7195af13d97edf172b596e94
JorgeLopez-Ceron <jorge.lopezceron742@gmail.com>
2024-12-07 01:04:53 -0600

    added info screen and fixed up button class

--- logic.py
+++ logic.py
@@ -2,16 +2,27 @@
 
 import pygame
 
+text_font = pygame.font.Font('GemunuLibre-VariableFont_wght.ttf', 40)
+
 class Button:
-    def __init__(self, x, y, image,scale):
-        width = image.get_width()
-        height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x,y)
+    def __init__(self, x, y, width, height, color, text, font, text_color):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.text = text
+        self.font = font
+        self.text_color = text_color
         self.clicked = False
 
     def draw(self,surface):
+        #draw button
+        pygame.draw.rect(surface, self.color, self.rect)
+
+        #draw text on buttons
+        text_surface = self.font.render(self.text, True, self.text_color)
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        surface.blit(text_surface, text_rect)
+
+        #check for click
         action = False
         #grabs mouse position
         pos = pygame.mouse.get_pos()
@@ -23,21 +34,21 @@ class Button:
 
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
-
-        surface.blit(self.image, (self.rect.x, self.rect.y))
-
         return action
+
+def bg(surface,image):
+    pass
+
 
 def menu_screen(surface,image,button):
     run = True
     button_clicked = False
-    button_done = False
 
 #code for title
-    font = pygame.font.Font('PaytoneOne-Regular.ttf', 50)
+    font = pygame.font.Font('BungeeTint-Regular.ttf', 50)
     title = font.render('Crack The Code', True, (255,255,255))
 
-
+    # menu game loop
     while run:
         surface.blit(image, (0, 0))
 
@@ -45,12 +56,15 @@ def menu_screen(surface,image,button):
             surface.blit(title, (surface.get_width() // 2 - title.get_width() // 2, 50))
             if button.draw(surface):
                 button_clicked = True
-                print('button has been clicked')
 
-        if button_clicked and not button_done:
-            #here we can call function to change screen
-            print('showing background')
-            button_done = True
+        if button_clicked:
+            #here we can call function to change screen and text
+            info_text_pt1 = text_font.render('Find the letters to create the code' ,True,(255,225,225))
+            surface.blit(info_text_pt1, (250 - info_text_pt1.get_width() // 2, 100))
+            info_text_p2 = text_font.render('to return to your world!',True , (255,225,225))
+            surface.blit(info_text_p2, (250 - info_text_p2.get_width() // 2, 150))
+            info_text_press_s = text_font.render('Press space to continue', True , (255,225,225))
+            surface.blit(info_text_press_s, (250 - info_text_press_s.get_width() // 2, 250))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
+pygame.init()
 import logic
 
 #code to make screen
-pygame.init()
 screen_width = 500
 screen_height = 500
 screen = pygame.display.set_mode((screen_width,screen_height))
@@ -12,13 +12,21 @@ pygame.display.set_caption('Crack The Code')
 status = 'main'
 
 #code for the start and background image
-start_image = pygame.image.load('start_button.png').convert_alpha()
 background_image = pygame.image.load('background_image.jpg')
 
 
 #loading button
-start_button = logic.Button(175,225, start_image, 0.1)
+start_button = logic.Button(
+    screen_width // 2 - 50,
+    screen_height // 2,
+100,
+50,
+(255,0,0),
+'START!',
+    pygame.font.Font('GemunuLibre-VariableFont_wght.ttf', 40),
+    (0,0,0)
+)
 
-#pygame.quit()
+#calling menu function
 logic.menu_screen(screen,background_image,start_button)
 
